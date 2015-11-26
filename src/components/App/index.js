546056fd395182e804aa9c5c0bc36b5fc3d1fb2a
Ellis Clayton <ellis@ellisclayton.info>
2015-11-26 19:57:06 +1000

    Individual static counters

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,17 +5,48 @@ var React = require('react');
 module.exports = React.createClass({
   displayName: 'App',
 
-  getInitialState: function() {
+  getInitialState: function () {
     return {
       total: 0
     }
   },
 
-  render: function() {
+  render: function () {
     return (
       <div className="App">
         <h1 className="App__header">Counter App</h1>
+        <h2>Total: {this.state.total}</h2>
+
+        <Counter name="Cats"/>
+        <Counter name="Dogs"/>
       </div>
     );
   },
+});
+
+
+var Counter = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0
+    };
+  },
+
+  decrement: function () {
+    this.setState({count: this.state.count - 1});
+  },
+
+  increment: function () {
+    this.setState({count: this.state.count + 1});
+  },
+
+  render: function () {
+    return (
+      <div className="counter">
+        {this.props.name}: {this.state.count}&nbsp;
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
 });
